ef189f90f74c127dbf00a03de17ed3d831b6005e
DD <DD@gmail.com>
2024-04-29 09:48:40 +0800

    update

---
+++ N20240429.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 09:30:24 2024
+
+@author: USER
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x =np.arange(0,5,0.1)
+y = np.square(x)
+plt.plot(x,y)
+plt.show()
